1664b5461163efb7261db3b0ceb2ca7c0b463a07
Eric Oestrich <eric@oestrich.org>
2022-05-05 00:32:57 -0400

    Add safe for the layout inner content

--- priv/templates/lib/app/web/layout.ex
+++ priv/templates/lib/app/web/layout.ex
@@ -11,7 +11,7 @@ defmodule <%= @app_module %>.Web.Layout do
   def wrap(token) do
     case Token.response_header(token, "content-type") do
       ["text/html"] ->
-        assigns = %{inner_content: token.response_body}
+        assigns = %{inner_content: {:safe, token.response_body}}
         render(token, "app.html", assigns)
 
       _ ->
