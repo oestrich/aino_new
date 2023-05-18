b145c44ffe52b8bab381f91af74595e552694335
Eric Oestrich <eric@oestrich.org>
2023-05-18 10:53:51 -0400

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule AinoNew.MixProject do
   def project do
     [
       app: :aino_new,
-      version: "0.5.0",
+      version: "0.6.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       description: "Installer and new project generator for Aino",
